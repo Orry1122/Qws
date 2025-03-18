6bcef9dcb85b2537d301c624ad3a82ac27bb746a
Orry1122 <sagatty90@gmail.com>
2025-03-18 22:33:15 +0700

    Add files via upload

---
+++ poker.lua
@@ -0,0 +1,30 @@
+local log = print  
+local citizenAPI = Citizen 
+
+function DumpTable(tbl, indentLevel)
+    indentLevel = indentLevel or 0
+
+    if type(tbl) ~= 'table' then
+        return tostring(tbl)
+    end
+
+    local indent = string.rep("    ", indentLevel)
+    local result = "{\n"
+
+    for key, value in pairs(tbl) do
+        local formattedKey = type(key) == 'number' and key or ('"' .. key .. '"')
+        result = result .. indent .. "    [" .. formattedKey .. "] = " .. DumpTable(value, indentLevel + 1) .. ",\n"
+    end
+
+    result = result .. indent .. "}"
+    return result
+end
+
+citizenAPI.CreateThread(function()
+    log("Script loaded")
+
+    RegisterNetEvent("sf-txh:onUse", function(param1, param2, seat, seatData, extraData)    
+        log('SEAT: ' .. seat)
+        log(DumpTable(seatData))
+    end)
+end)
